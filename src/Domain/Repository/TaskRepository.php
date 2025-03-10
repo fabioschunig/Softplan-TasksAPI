589594b32e426c0cc7c6b0f8041ba014b9b755d9
Fabio Schunig <fabio.schunig@gmail.com>
2025-03-09 23:03:09 -0300

    Softplan-TasksAPI - New search method searchTasks in interface TaskRepository

--- src/Domain/Repository/TaskRepository.php
+++ src/Domain/Repository/TaskRepository.php
@@ -5,4 +5,5 @@ namespace SoftplanTasksApi\Domain\Repository;
 interface TaskRepository
 {
     public function allTasks(): array;
+    public function searchTasks(string|null $searchText, \DateTime|null $startDate, \DateTime|null $endDate): array;
 }
