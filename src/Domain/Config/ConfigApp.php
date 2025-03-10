0841b9880ae2ae43381d0e39e4a4aebd26ac96af
Fabio Schunig <fabio.schunig@gmail.com>
2025-03-09 22:52:33 -0300

    Softplan-TasksAPI - New functions to get config params in ConfigApp interface

--- src/Domain/Config/ConfigApp.php
+++ src/Domain/Config/ConfigApp.php
@@ -5,4 +5,9 @@ namespace SoftplanTasksApi\Domain\Config;
 interface ConfigApp
 {
     public function loadEnv();
+
+    public function getHost(): string;
+    public function getDBName(): string;
+    public function getUsername(): string;
+    public function getPassword(): string;
 }
