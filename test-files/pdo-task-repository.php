8c2e0a4830b02d05ca3b7d1bff3e927b0f419c51
Fabio Schunig <fabio.schunig@gmail.com>
2025-03-09 23:16:26 -0300

    Softplan-TasksAPI - Testing method allTasks empty

--- test-files/pdo-task-repository.php
+++ test-files/pdo-task-repository.php
@@ -1,8 +1,16 @@
 <?php
 
+use SoftplanTasksApi\Infrastructure\Repository\PdoTaskRepository;
+
 require_once 'vendor/autoload.php';
 
 include_once 'pdo-connection.php';
 
 echo "Connection: \n";
 var_dump($pdoConnection);
+
+$taskRepository = new PdoTaskRepository();
+
+$tasks = $taskRepository->allTasks();
+echo "All Tasks: \n";
+var_dump($tasks);
