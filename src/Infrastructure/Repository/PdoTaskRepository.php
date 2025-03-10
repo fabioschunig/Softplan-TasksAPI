07e78c86e1d7873f497a1a5efd91fcb5c187125f
Fabio Schunig <fabio.schunig@gmail.com>
2025-03-09 23:31:40 -0300

    Softplan-TasksAPI - Get data for allTasks method from TAREFA table

--- src/Infrastructure/Repository/PdoTaskRepository.php
+++ src/Infrastructure/Repository/PdoTaskRepository.php
@@ -3,6 +3,7 @@
 namespace SoftplanTasksApi\Infrastructure\Repository;
 
 use SoftplanTasksApi\Domain\Repository\TaskRepository;
+use SoftplanTasksApi\Domain\Model\Task;
 use PDO;
 
 class PdoTaskRepository implements TaskRepository
@@ -16,7 +17,27 @@ class PdoTaskRepository implements TaskRepository
 
     public function allTasks(): array
     {
-        return array();
+        $sqlQuery = 'SELECT * FROM tarefa;';
+        $stmt = $this->connection->query($sqlQuery);
+
+        $taskDataList = $stmt->fetchAll();
+        $taskList = [];
+
+        foreach ($taskDataList as $taskData) {
+            $task = new Task(
+                $taskData['id'],
+                $taskData['descricao'],
+                $taskData['referencia'] ? null : new \DateTime($taskData['referencia']),
+                $taskData['inicio'] ? null : new \DateTime($taskData['inicio']),
+                $taskData['fim'] ? null : new \DateTime($taskData['fim']),
+                $taskData['observacao'],
+                $taskData['origem'],
+            );
+
+            $taskList[] = $task;
+        }
+
+        return $taskList;
     }
 
     public function searchTasks(string|null $searchText, \DateTime|null $startDate, \DateTime|null $endDate): array
