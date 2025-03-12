0d19a060777ae30841792bb49f37b80ff369ca93
Fabio Schunig <fabio.schunig@gmail.com>
2025-03-11 22:34:34 -0300

    Softplan-TasksAPI - API call with all tasks in JSON format

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
 echo "Softplan - Tasks API";
+
+?>
+
+<div>
+    <p><a href="task.api.php">API call example</a></p>
+</div>
--- public/task.api.php
+++ public/task.api.php
@@ -1,3 +1,30 @@
 <?php
 
-echo "Task API v1";
+use SoftplanTasksApi\Application\Config\ConfigAppEnvFile;
+use SoftplanTasksApi\Infrastructure\Repository\PdoTaskRepository;
+
+require_once '../vendor/autoload.php';
+
+$config = new ConfigAppEnvFile;
+$config->loadEnv();
+
+$pdoConnection = \SoftplanTasksApi\Infrastructure\Persistence\PdoConnectionCreator::createConnection(
+    $config->getHost(),
+    $config->getDBName(),
+    $config->getUsername(),
+    $config->getPassword(),
+);
+
+$taskRepository = new PdoTaskRepository(
+    $pdoConnection,
+    $config->getResponsavel(),
+    $config->getProjeto(),
+);
+
+$tasks = $taskRepository->allTasks();
+
+// API response in JSON
+header('Content-Type: application/json');
+echo json_encode([
+    'tasks' => $tasks,
+]);
