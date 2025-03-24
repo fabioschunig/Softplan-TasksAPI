ba840a3847b033dd136f457f912573739af87eff
Fabio Schunig <fabio.schunig@gmail.com>
2025-03-24 17:25:10 -0300

    Softplan-TasksAPI - Task table in MySQL script file

--- database/mysql.sql
+++ database/mysql.sql
@@ -3,3 +3,13 @@
 CREATE DATABASE softplan_task_api;
 
 use softplan_task_api;
+
+CREATE TABLE `task` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `description` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tags` varchar(256) COLLATE utf8mb4_unicode_ci NULL,
+  `status` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+commit;
