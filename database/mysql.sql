04bfe668824e5b3c34e67fd0af7dfbc85ee0a3bf
Fabio Schunig <fabio.schunig@gmail.com>
2025-04-01 20:37:18 -0300

    Softplan-TasksAPI - Ajustando tabelas Project e Task

--- database/mysql.sql
+++ database/mysql.sql
@@ -5,19 +5,28 @@ CREATE DATABASE softplan_task_api;
 use softplan_task_api;
 
 CREATE TABLE `project` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `description` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(256) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `task` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `description` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tags` varchar(256) COLLATE utf8mb4_unicode_ci NULL,
-  `status` int(1) NOT NULL DEFAULT 0,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tags` VARCHAR(256) COLLATE utf8mb4_unicode_ci NULL,
+  `project_id` INT UNSIGNED NULL,
+  `started` TIMESTAMP NULL,
+  `finished` TIMESTAMP NULL,
+  `status` TINYINT(1) NOT NULL DEFAULT 0,
+  `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `task`
+  ADD CONSTRAINT `fk_task_project` FOREIGN KEY (`project_id`)
+  REFERENCES `project` (`id`);
 
 commit;
