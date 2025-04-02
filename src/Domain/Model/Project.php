492069bc1e92804ddb0e616d69e7a84b9e55685b
Fabio Schunig <fabio.schunig@gmail.com>
2025-04-02 20:45:26 -0300

    Softplan-TasksAPI - Campos de data no modelo Project

--- src/Domain/Model/Project.php
+++ src/Domain/Model/Project.php
@@ -6,12 +6,18 @@ class Project
 {
     public readonly int $id;
     public readonly string $description;
+    public readonly \DateTime|null $created;
+    public readonly \DateTime|null $updated;
 
     public function __construct(
         int $id,
         string $description,
+        \DateTime|null $created,
+        \DateTime|null $updated,
     ) {
         $this->id = $id;
         $this->description = $description;
+        $this->created = $created;
+        $this->updated = $updated;
     }
 }
