d2dd46c6fb357e94ef3899bf3b017aee2a915eb7
Fabio Schunig <fabio.schunig@gmail.com>
2025-04-07 22:45:13 -0300

    Softplan-TasksAPI - Ajuste na quebra de parâmetros

--- src/Domain/Repository/TaskRepository.php
+++ src/Domain/Repository/TaskRepository.php
@@ -5,5 +5,9 @@ namespace SoftplanTasksApi\Domain\Repository;
 interface TaskRepository
 {
     public function allTasks(): array;
-    public function searchTasks(string|null $searchText, \DateTime|null $startDate, \DateTime|null $endDate): array;
+    public function searchTasks(
+        string|null $searchText,
+        \DateTime|null $startDate,
+        \DateTime|null $endDate
+    ): array;
 }
