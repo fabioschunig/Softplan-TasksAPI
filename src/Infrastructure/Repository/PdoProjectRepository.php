556520e11ad41c8f97d1866de3ba634f11132abb
Fabio Schunig <fabio.schunig@gmail.com>
2025-05-01 21:59:25 -0300

    Softplan-TasksAPI - Implementando método obrigatórios na PdoProjectRepository

--- src/Infrastructure/Repository/PdoProjectRepository.php
+++ src/Infrastructure/Repository/PdoProjectRepository.php
@@ -2,4 +2,23 @@
 
 namespace SoftplanTasksApi\Infrastructure\Repository;
 
-class PdoProjectRepository {}
+use SoftplanTasksApi\Domain\Repository\ProjectRepository;
+
+class PdoProjectRepository implements ProjectRepository
+{
+
+    public function allProjects(): array
+    {
+        // Implement logic to retrieve all projects
+        return [];
+    }
+
+    public function searchProjects(
+        string|null $searchText,
+        \DateTime|null $startDate,
+        \DateTime|null $endDate
+    ): array {
+        // Implement logic to retrieve all projects
+        return [];
+    }
+}
